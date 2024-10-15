50c0f430b877ea542f571ed1474dcf4a9323fa5b
roblou2 <robhamburg89@gmail.com>
2024-10-15 23:55:35 +0100

    added logic for building a player with a hand and ability to add card to hands upon calling a hit function

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
-//make a random player hand for start of game
 
-const makeCard = () => {
+//define suits and cards
 
-    let suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
-    let unsuitedCards = ["Jack", "Queen", "King", "Ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-    
+let suits = ["Clubs", "Spades", "Hearts", "Diamonds"]
+let unsuitedCards = ["Jack", "Queen", "King", "Ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+
+
+//define a function to make a card
+
+const makeCard= () => {
+
     const randomSuit = () => Math.floor(Math.random() * 4); //between 0 and 3
     const random = () =>  Math.floor(Math.random() * 13); //between 0 and 12
     
-        const card = () => {
+    
     
         const getSuit = () => suits[randomSuit()]
         const getUnsuitedCard = () => unsuitedCards[random()]
     
-         return getUnsuitedCard() + " of " + getSuit() 
-        }
-        return card()
+         return `${getUnsuitedCard() + " of " + getSuit()}`
+
     }
 
-const hand = {
-    cardOne: makeCard(),
-    cardTwo: makeCard()
+    //define a function to make a hand for start of game
+
+    const makeHand = () => {
+        return {
+            cardOne: makeCard(),
+            cardTwo: makeCard()
+        };
+    };
+
+//create factory function to make player, add makeCard method
+// and get their hand
+const createPlayer = (name, hand) => {
+
+    const cards =  [hand.cardOne, hand.cardTwo] //initialise start
+    //of game with 2 cards for the hand
+
+    //define method to make a random card
+
+
+    let getPlayerName = () => name
+
+    let hit = () => {
+        
+      let newCard =  makeCard()
+     cards.push(newCard)
+    }
+    return {getPlayerName, cards, hit}
+    }
+    
+
+    const player = createPlayer("rob", makeHand()) //get name from frontend
+
+
+    //define a function to add a card to player's hand
+const hit = () => {
+  return player.hit()
 }
 
+//define a function to make game over if hand is above 21
 
-//create factory function to make player and get their hand
-    const createPlayer = (name, hand) => {
 
-let getPlayerName = () => name
-
-let getPlayerHand = () => hand
-
-return {getPlayerName, getPlayerHand}
-}
-const rob = createPlayer("rob", hand)
-console.log(rob.getPlayerHand())
